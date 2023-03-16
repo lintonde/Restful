90e360b08c57505a2c54287b4adb9eb9abd573b1
Avi Assayag <avi.assayag@gmail.com>
2023-03-16 18:41:11 +0200

    adding gitignore and more

--- index.js
+++ index.js
@@ -4,10 +4,7 @@
 const https = require('https');
 const fs = require('fs');
 const express = require('express');
-const { google } = require('googleapis');
-const { OAuth2Client } = require('google-auth-library');
-const passport = require('passport');
-const OAuth2Strategy = require('passport-oauth2').Strategy;
+const router = express.Router();
 const bodyParser = require('body-parser');
 const request = require('request');
 const cors = require('cors');
@@ -36,105 +33,7 @@ app.use(cors());
 (async () => {
   try {
     const google_api = {
-      auth: function() {
-        const fs = require('fs');
-        const path = require('path');
-        const process = require('process');
-        const { authenticate } = require('@google-cloud/local-auth');
-        const { google } = require('googleapis');
-
-        // If modifying these scopes, delete token.json.
-        const SCOPES = [
-          'https://www.googleapis.com/auth/spreadsheets'
-        ];
-        // The file token.json stores the user's access and refresh tokens, and is
-        // created automatically when the authorization flow completes for the first
-        // time.
-        const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-        const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
-        /**
-         * Reads previously authorized credentials from the save file.
-         *
-         * @return {Promise<OAuth2Client|null>}
-         */
-         function loadSavedCredentialsIfExist() {
-          try {
-            const content =  fs.readFile(TOKEN_PATH);
-            const credentials = JSON.parse(content);
-            return google.auth.fromJSON(credentials);
-          } catch (err) {
-            return null;
-          }
-        }
-
-        /**
-         * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
-         *
-         * @param {OAuth2Client} client
-         * @return {Promise<void>}
-         */
-         function saveCredentials(client) {
-          const content =  fs.readFile(CREDENTIALS_PATH);
-          const keys = JSON.parse(content);
-          const key = keys.installed || keys.web;
-          const payload = JSON.stringify({
-            type: 'authorized_user',
-            client_id: key.client_id,
-            client_secret: key.client_secret,
-            refresh_token: client.credentials.refresh_token,
-          });
-           fs.writeFile(TOKEN_PATH, payload);
-        }
-
-        /**
-         * Load or request or authorization to call APIs.
-         *
-         */
-         function authorize() {
-          let client =  loadSavedCredentialsIfExist();
-          console.log(client);
-          return;
-          if (client) {
-            return client;
-          }
-          client =  authenticate({
-            scopes: SCOPES,
-            keyfilePath: CREDENTIALS_PATH,
-          });
-          if (client.credentials) {
-             saveCredentials(client);
-          }
-          console.log(client);
-          return client;
-        }
-
-        return  authorize();
-      },
-      addTracking:  () => {
-       const auth = authorize();
-        if (auth) {
-          const title = "test1"
-          const service = google.sheets({ version: 'v4', auth });
-          const resource = {
-            properties: {
-              title,
-            },
-          };
-          try {
-            console.log(service);
-            const spreadsheet = servic.create({
-              resource,
-              fields: 'spreadsheetId',
-            });
-            console.log(`Spreadsheet ID: ${spreadsheet.data.spreadsheetId}`);
-            return spreadsheet.data.spreadsheetId;
-          } catch (err) {
-            // TODO (developer) - Handle exception
-            throw err;
-          }
-        }
-      }
+     
     };
 
     // objects to use
