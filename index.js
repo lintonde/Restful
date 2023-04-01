44172b7f93e83c8d8c0b39b9df66e5d964cc3021
Avi Assayag <avi.assayag@gmail.com>
2023-04-01 19:01:17 +0300

    email service

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 // imports
 const https = require('https');
-const fs = require('fs');
+const fs = require('fs').promises;
 const express = require('express');
-const router = express.Router();
+var { authorize, appendEvent, checkIfClientExists, createClient, getSpreadSheetForRest } = require("./funcs");
+const { finishMongo, newMongoEvent, newMongoRest, weekMongo, sendWeekMail, allRests } = require("./mongo_functions");
 const bodyParser = require('body-parser');
 const request = require('request');
-const mongo = require("./mongo"); 
+const path = require("path");
+const CLIENTS_PATH = path.join(process.cwd(), "clients.json");
 const cors = require('cors');
 const app = express();
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
@@ -32,6 +34,7 @@ app.use(cors());
 // Restful API
 (async () => {
   try {
+
     // objects to use
     const woo_api = new WooCommerceRestApi({
       url: "https://restful.co.il",
@@ -204,7 +207,196 @@ app.use(cors());
       writeProductsJson();
     });
 
-    
+    app.get("/api/createNewClient", async (req, res) => {
+      /**
+       * Create a google spreadsheet
+       * @param {string} title Spreadsheets title
+       * @return {string} Created spreadsheets ID
+       */
+      const restaurant = req.query.name;
+      const exists = await checkIfClientExists(restaurant);
+      if (exists != -1)
+        res.send("Client already exists");
+      else {
+        await createClient(restaurant);
+        res.send("Client's spreadsheeet created");
+      }
+    });
+
+    app.get("/api/newEvent", async (req, res) => {
+      try {
+        const { rest, clickType } = req.query;
+        const exists = await checkIfClientExists(rest);
+        let spreadId;
+        if (exists == -1) spreadId = await createClient(rest);
+        else spreadId = exists;
+
+        const client = await authorize();
+        await appendEvent(client, spreadId, clickType);
+        res.send("Event created");
+      } catch (e) {
+        console.log("error creating event", e);
+        res.send("error");
+      }
+    });
+
+    app.get("/api/finish", async (req, res, next) => {
+      const client = await authorize();
+      const clients = await fs.readFile(CLIENTS_PATH);
+      let map = new Map(Object.entries(JSON.parse(clients)));
+      let data = [];
+      let list = [];
+      let call = [];
+      let wolt = [];
+      let tenBis = [];
+      let mishlocha = [];
+      let google = [];
+      let easy = [];
+      let facebook = [];
+      let instagram = [];
+      let zapRest = [];
+      let veganFriendly = [];
+      let tripAdvisor = [];
+      let ontapo = [];
+      let waze = [];
+
+      map.forEach(async (id, rest) => {
+        data = await getSpreadSheetForRest(client, id);
+        const month = new Date().getMonth();
+        list = [];
+        if (data) {
+          call = [];
+          wolt = [];
+          tenBis = [];
+          mishlocha = [];
+          google = [];
+          easy = [];
+          facebook = [];
+          instagram = [];
+          zapRest = [];
+          veganFriendly = [];
+          tripAdvisor = [];
+          ontapo = [];
+          waze = [];
+          let m;
+          data.forEach((array) => {
+            m = new Date(array[2]).getMonth();
+            if (m == month - 1) {
+              switch (array[0]) {
+                case "call":
+                  call.push(array);
+                  break;
+
+                case "wolt":
+                  wolt.push(array);
+                  break;
+
+                case "tenBis":
+                  tenBis.push(array);
+                  break;
+
+                case "mishlocha":
+                  mishlocha.push(array);
+                  break;
+
+                case "google":
+                  google.push(array);
+                  break;
+
+                case "easy":
+                  easy.push(array);
+                  break;
+
+                case "facebook":
+                  facebook.push(array);
+                  break;
+                case "instagram":
+                  instagram.push(array);
+                  break;
+
+                case "zapRest":
+                  zapRest.push(array);
+                  break;
+                case "veganFriendly":
+                  veganFriendly.push(array);
+                  break;
+
+                case "tripAdvisor":
+                  tripAdvisor.push(array);
+                  break;
+                case "ontapo":
+                  ontapo.push(array);
+                  break;
+                case "waze":
+                  waze.push(array);
+                  break;
+                default:
+                  break;
+              }
+            }
+          });
+
+          var objectToSend = {
+            call: call.length,
+            wolt: wolt.length,
+            tenBis: tenBis.length,
+            mishlocha: mishlocha.length,
+            google: google.length,
+            easy: easy.length,
+            facebook: facebook.length,
+            instagram: instagram.length,
+            zapRest: zapRest.length,
+            veganFriendly: veganFriendly.length,
+            tripAdvisor: tripAdvisor.length,
+            ontapo: ontapo.length,
+            waze: waze.length,
+          };
+          console.log("rest", rest);
+          console.log("list", objectToSend);
+        }
+      });
+
+      res.send("thanx");
+    });
+
+    app.get("/api/newRest", async (req, res) => {
+      const shop = req.query.shop;
+      const result = await newMongoRest(shop);
+      res.send(result);
+    });
+
+    app.get("/api/newMongoEvent", async (req, res) => {
+      const { event, rest } = req.query;
+      const result = await newMongoEvent(event, rest);
+      res.send(result);
+    });
+
+    app.get("/api/mongoFinish", async (req, res) => {
+      const rest = req.query.rest;
+      const oToSend = await finishMongo(rest);
+      res.send(oToSend);
+    });
+
+    app.post('/sendRest', async (req, res) => {
+      const { title, titleHeb, email } = req.body
+      console.log(req.body)
+      if (title && titleHeb && email) {
+        const result = await newMongoRest(req.body);
+        res.send("Shop succefuly entered to DB")
+      }
+      else
+        res.send("missing details")
+
+    })
+
+    app.get("/api/MongoWeek", async (req, res) => {
+
+      // const rest = req.query.rest
+      //const oToSend = await weekMongo(rest)
+      // sendWeekMail(oToSend)
+      allRests();
+      res.send('thanks');
+    });
 
     app.listen(process.env.PORT || 80, () => {
       console.log('listen to port 80');
